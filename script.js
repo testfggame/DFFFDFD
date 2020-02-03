48043be01bb2c02ac26526ba6e0d85a30dc6f4af
testfggame <51860629+testfggame@users.noreply.github.com>
2020-02-04 00:24:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>♧ Ꮢocket 🚀</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+<link rel="icon" 
+      type="image/png" 
+      href="discord.png">
+	  </link>
+  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css'>
+
+      <link rel="stylesheet" href="style.css">
+
+  
+</head>
+
+<body>	
+
+<div class="container d-flex flex-column">
+  <div class="header d-flex flex-column justify-content-between align-items-center">
+    <nav class="topmenu d-flex justify-content-center"><a href="#about">About</a>
+      <div class="line align-self-center"></div><a href="#rules">Discord Rules</a>
+      <div class="line align-self-center"></div><a href="[YTChannel]">YouTube</a>
+      <div class="line align-self-center"></div><a href="[TwitterLink]">Twitter</a>
+      <div class="line align-self-center"></div><a href="https://discord.gg/v3bXUtS">Join The Discord</a>
+    </nav>
+    <div class="server-info d-flex align-items-center flex-column text-center">
+      <h1>♧ Ꮢocket 🚀</h1><a class="links-suppressor join-btn btn-pill white" href="https://discord.gg/v3bXUtS">Join The Discord</a>
+    </div>
+  </div>
+  <div class="inner d-flex flex-column">
+    <section class="d-flex justify-content-between flex-lg-nowrap flex-wrap" id="about">
+      <div class="image image1" style="flex-grow: 1;"></div>
+      <div class="text">
+	  <div class="alert">
+ </section>
+    <section class="discord-widget">
+      <iframe src="https://discordapp.com/widget?id=672474391006806062&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe></iframe>
+    </section>
+    <section class="d-flex justify-content-between flex-lg-nowrap flex-wrap flex-row-reverse" id="rules">
+      <div class="image image2" style="flex-grow: 1;"></div>
+      <div class="text">
+        <h2>القوانين</h2>
+        <p><b>The rules are as followed</b>&nbsp;<br/><br/><b>but are not limited to:</b><br/></p>
+        <ul>
+          <li>لا إعلانات ، ما لم يتم السماح لك</li>
+          <li>لا محتوى غير مناسب</li>
+          <li>لا للسبام</li>
+          <li>لا تضيع وقت الموظفين </li>
+          <li>لا تقم بأعمال الشغب</li>
+          <li>لا تقم بتهديد [أو تقوم بالقتل أو التهديد الخبيث] (أو تكشف عن معلومات شخصية في أي شكل أو شكل)</li>
+          <li>لا تطلب رتبه بـ اي شكل</li>
+		  <li>لا تميز أو تزعج أي شخص بأي شكل أو شكل</li>
+		  <li>لا صور ملف شخصي غير مناسب</li>
+		  <li>لا للدردشه الغير مناسبه</li>
+
+        </ul>
+		<br></br>
+      </div>
+    </section>
+	        <h3>Videos / Clips:</h3>
+			<div class="row">
+    <div class="col-sm-4"><iframe width="320" height="165" src="https://www.youtube.com/embed/[YTID1]" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+  <div class="col-sm-4"><iframe width="320" height="165" src="https://www.youtube.com/embed/[YTID2]" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+  <div class="col-sm-4"><iframe width="320" height="165" src="https://www.youtube.com/embed/Ywz1lWiTI5o" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+
+  </div>
+    <section class="join d-flex justify-content-center">
+      <div class="line align-self-center" style="flex-grow: 1;"></div><a class="links-suppressor btn-pill white" href="https://discord.gg/v3bXUtS">Join The Discord</a>
+      <div class="line align-self-center" style="flex-grow: 1;"></div>
+    </section>
+	    <nav class="topmenu d-flex justify-content-center"><a href="#about">About</a>
+      <div class="line align-self-center"></div><a href="#rules">Discord Rules</a>
+      <div class="line align-self-center"></div><a href="[YTChannel]">YouTube</a>
+      <div class="line align-self-center"></div><a href="[TwitterLink]">Twitter</a>
+      <div class="line align-self-center"></div><a href="https://discord.gg/v3bXUtS">Join The Discord</a>
+    </nav>
+  </div>
+  <div class="footer d-flex justify-content-between flex-lg-nowrap flex-wrap">
+    <div class="d-flex">
+      <div class="block d-flex flex-column justify-content-between">
+        <h3 class="d-flex">Owner</h3><span> ibrahem ,♧ Ꮢocket 🚀
+#6372</span><span></span>𝑋𝑀𝐼𝑁𝐸_𝐺𝐴𝑀𝐼𝑁𝐺™
+#9864<span></span><span></span><span></span>
+      </div>
+      <div class="block d-flex flex-column justify-content-between">
+        <h3 class="d-flex">Co-Owner</h3><span>!『ITS』I Shadobom?!!
+#8631</span><span></span><span></span><span></span><span></span>
+      </div>
+    </div>
+    <p class="align-self-end"><var>♧ Ꮢocket 🚀</var></p>
+  </div>
+</div>
+  
+  
+
+</body>
+
+</html>
